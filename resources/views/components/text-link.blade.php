c64b0aa4d1e2302be08fa1fbad41bb5414c84361
7kaGma <168641333+7kaGma@users.noreply.github.com>
2024-06-30 10:04:36 +0900

    Revert "Feature/component"

--- resources/views/components/text-link.blade.php
+++
@@ -1,3 +0,0 @@
-<a {{ $attributes->merge(['href' => '$href', 'class' => 'link']) }}>
-    {{ $slot }}
-</a>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,23 +6,14 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo width="100" class="block" />
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <!-- <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link> -->
-                    <x-nav-link :href="route('index')" :active="request()->routeIs('index')">
-                        提案書作成
-                    </x-nav-link>
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        投稿一覧
-                    </x-nav-link>
-                    <x-nav-link :href="route('sample')" :active="request()->routeIs('sample')">
-                        マイページ
+                        {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -76,15 +67,9 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-nav-link :href="route('index')" :active="request()->routeIs('index')">
-                提案書作成
-            </x-nav-link>
-            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                投稿一覧
-            </x-nav-link>
-            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                マイページ
-            </x-nav-link>
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                {{ __('Dashboard') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
